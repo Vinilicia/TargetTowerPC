6a39527ff1a74633a29960762776f679b9309a73
Hawkeye-789 <dyegoeduardo0032@gmail.com>
2025-08-28 15:37:20 -0300

    Funcionamento dos ataques completo

--- Scripts/Actors/Enemies/normal_goblin.gd
+++ Scripts/Actors/Enemies/normal_goblin.gd
@@ -1,13 +1,17 @@
 extends CharacterBody2D
 
+# ======================
+# CONSTANTES
+# ======================
 const DASH_DURATION : float = 0.4
 const DASH_SPEED_MULTIPLIER : float = 2.0
-const CHASE_PERSISTENCE_TIME : float = 1.0
 const LOOK_AROUND_TIME : float = 2.0
 const LOOK_AROUND_AREA_INCREASE : float = 2
-const GIVING_UP_CHASE_TIME : float = 2.0
 const ARROW_AVOID_DELAY : float = 0.15
 
+# ======================
+# EXPORTS
+# ======================
 @export_enum("Esquerda", "Direita") var start_direction: int
 @export var walk_speed: float
 @export var chase_speed: float
@@ -15,23 +19,33 @@ const ARROW_AVOID_DELAY : float = 0.15
 @export var v_component : VelocityComponent
 @export var direction_change_delay : float = 0.3
 @export var lines_of_sight : Array[RayCast2D]
+@export var hurtbox: Hurtbox
+@export var slash_hitbox: Hitbox
 
+# ======================
+# ONREADY
+# ======================
 @onready var state_chart: StateChart = $StateChart
 
 @onready var wall_detector: RayCast2D = $Detectors/WallDetector
 @onready var jump_detector: RayCast2D = $Detectors/JumpDetector
 @onready var fall_detector: RayCast2D = $Detectors/FallDetector
 @onready var ground_detector: RayCast2D = $Detectors/GroundDetector
+
 @onready var jump_attack_area: Area2D = $Areas/JumpAttackArea
 @onready var stab_attack_area: Area2D = $Areas/StabAttackArea
 @onready var sight_area: Area2D = $Areas/SightArea
 @onready var outer_arrow_detec: Area2D = $Areas/OuterArrowDetector
 @onready var inner_arrow_detec: Area2D = $Areas/InnerArrowDetector
+
 @onready var coll: CollisionShape2D = $Coll
+@onready var giving_up_timer : Timer = $Timers/GivingUpTimer
+@onready var jump_attack_timer : Timer = $Timers/JumpAttackTimer
+@onready var stab_attack_timer : Timer = $Timers/StabAttackTimer
 
-@export var hurtbox: Hurtbox
-@export var slash_hitbox: Hitbox
-
+# ======================
+# VARIÁVEIS DE ESTADO
+# ======================
 var player_target: CharacterBody2D
 var player_relative_position: Vector2
 
@@ -39,6 +53,7 @@ var direction: int = 1
 var is_player_inside: bool = false
 var player_is_nearby: bool = false
 var saw_player: bool = false
+
 var can_dash: bool = true
 var outer_detector : bool = false
 var inner_detector : bool = false
@@ -46,20 +61,21 @@ var is_changing_direction := false
 var is_jumping : bool = false
 var still_cant_react : bool = false
 var saw_arrow : bool = false
-var attacking : bool = false
 
+var attacking : bool = false
+var can_jump_attack : bool = true
+var can_stab_attack : bool = true
+var player_in_range : bool = false
+var start_attack : bool = false
+
+
+# ======================
+# CICLO DE VIDA
+# ======================
 func _ready() -> void:
-	
-	# Normaliza a direção inicial
 	direction = -1 if start_direction == 0 else 1
 	if direction == 1:
 		flip()
-
-func get_current_gravity() -> Vector2:
-	if attacking:
-		return get_gravity() / 2
-	else:
-		return get_gravity()
 
 func _physics_process(delta: float) -> void:
 	if not is_on_floor():
@@ -80,14 +96,19 @@ func _physics_process(delta: float) -> void:
 
 
 # ======================
+# GRAVIDADE
+# ======================
+func get_current_gravity() -> Vector2:
+	return get_gravity() / 2 if attacking else get_gravity()
+
+
+# ======================
 # DETECÇÃO DO PLAYER
 # ======================
-
 func _update_lines_of_sight() -> void:
 	for los in lines_of_sight:
 		player_relative_position = player_target.global_position - los.global_position
 		los.target_position = player_relative_position
-
 
 func _player_visible_in_sight() -> bool:
 	for los in lines_of_sight:
@@ -95,29 +116,27 @@ func _player_visible_in_sight() -> bool:
 			return true
 	return false
 
-
 func _start_chase() -> void:
 	state_chart.send_event("Chase_Player")
 	saw_player = true
 
 func _player_entered_sight_area(player: Node2D) -> void:
-	is_player_inside = true
 	assign_player(player)
+	if !giving_up_timer.is_stopped():
+		giving_up_timer.stop()
 
 func assign_player(player : Node2D) -> void:
 	player_target = player
 	player_is_nearby = true
 
 func unnasign_player() -> void:
-	if !is_player_inside:
-		player_is_nearby = false
-		saw_player = false
-		state_chart.send_event("Guard")
+	saw_player = false
+	state_chart.send_event("Guard")
 
 func _player_exited_sight_area(_player: Node2D) -> void:
-	is_player_inside = false
-	await get_tree().create_timer(CHASE_PERSISTENCE_TIME).timeout
-	unnasign_player()
+	player_is_nearby = false
+	if giving_up_timer.is_stopped():
+		giving_up_timer.start()
 
 func look_for_player() -> void:
 	_change_direction()
@@ -125,29 +144,29 @@ func look_for_player() -> void:
 	await get_tree().create_timer(LOOK_AROUND_TIME).timeout
 	sight_area.scale.x /= LOOK_AROUND_AREA_INCREASE
 
+
 # ======================
 # ATAQUE
 # ======================
-
 func _player_entered_jump_attack_area(_body: Node2D) -> void:
-	if is_on_floor():
+	if is_on_floor() and can_jump_attack:
+		start_attack = false
 		jump_attack()
+	else:
+		start_attack = true
 
-func jump_attack() -> void:
-	attacking = true
-	v_component.set_proper_velocity(Vector2(100 * direction, -80))
-	slash_hitbox.set_deferred("monitorable", true)
-	slash_hitbox.visible = true
-	await get_tree().create_timer(0.5).timeout
-	attacking = false
-	slash_hitbox.visible = false
-	slash_hitbox.set_deferred("monitorable", false)
+func _player_exited_jump_attack_area(body: Node2D) -> void:
+	start_attack = false
 
 func _player_entered_stab_attack_area(_body: Node2D) -> void:
-	if is_on_floor():
-		stab_attack()
+	player_in_range = true
+	state_chart.send_event("Attack")
 
-func stab_attack() -> void:
+func _player_exited_stab_attack_area(_body: Node2D) -> void:
+	player_in_range = false
+
+func attack() -> void:
+	print("__________")
 	attacking = true
 	slash_hitbox.set_deferred("monitorable", true)
 	slash_hitbox.visible = true
@@ -156,63 +175,96 @@ func stab_attack() -> void:
 	slash_hitbox.visible = false
 	slash_hitbox.set_deferred("monitorable", false)
 
-func _on_attack_state_physics_processing(delta: float) -> void:
-	if is_on_floor():
-		v_component.set_proper_velocity(0.0, 1)
+func jump_attack() -> void:
+	can_jump_attack = false
+	can_stab_attack = false
+	if !stab_attack_timer.is_stopped():
+		stab_attack_timer.stop()
+	v_component.set_proper_velocity(Vector2(100 * direction, -80))
+	attack()
+	_reset_cooldown(jump_attack_timer)
+	_reset_cooldown(stab_attack_timer)
+
+func stab_attack() -> void:
+	can_stab_attack = false
+	can_jump_attack = false
+	if !jump_attack_timer.is_stopped():
+		jump_attack_timer.stop()
+	attack()
+	_reset_cooldown(jump_attack_timer)
+	_reset_cooldown(stab_attack_timer)
+
+func _reset_cooldown(timer : Timer) -> void:
+	timer.start()
+
+func _on_jump_attack_timer_timeout() -> void:
+	can_jump_attack = true
+
+func _on_stab_attack_timer_timeout() -> void:
+	can_stab_attack = true
 
 # ======================
 # ESTADOS
 # ======================
-
 func jump() -> void:
 	is_jumping = true
 	v_component.set_proper_velocity(jump_force, 2)
 
+func _on_attack_state_physics_processing(delta: float) -> void:
+	if is_on_floor():
+		v_component.set_proper_velocity(0.0, 1)
+		if player_in_range and can_stab_attack:
+			stab_attack()
+	
+	if !player_in_range and !attacking:
+		state_chart.send_event("Chase")
+
 func _on_chasing_state_physics_processing(_delta: float) -> void:
 	v_component.set_proper_velocity(chase_speed * direction, 1)
-	
+
+	# detectores
 	wall_detector.force_raycast_update()
 	ground_detector.force_raycast_update()
 	jump_detector.force_raycast_update()
 	fall_detector.force_raycast_update()
+
 	var hit_wall := wall_detector.is_colliding()
 	var can_jump := !jump_detector.is_colliding()
 	var no_ground := !ground_detector.is_colliding()
 	var can_fall := fall_detector.is_colliding()
+
 	if (hit_wall or no_ground):
-		if hit_wall and can_jump and !is_jumping and abs(player_target.global_position.y - global_position.y) > 10  :
+		if hit_wall and can_jump and !is_jumping and abs(player_target.global_position.y - global_position.y) > 10:
 			jump()
 		elif no_ground and can_fall:
 			pass
 		else:
 			v_component.set_proper_velocity(0.0, 1)
-	
+
 	if not _player_visible_in_sight() and saw_player:
-		var giving_up_timer : Timer = Timer.new()
-		giving_up_timer.one_shot = true
-		giving_up_timer.autostart = false
-		giving_up_timer.wait_time = GIVING_UP_CHASE_TIME
-		giving_up_timer.timeout.connect(func():
-			saw_player = false
-			state_chart.send_event("Guard")
-			)
-		add_child(giving_up_timer)
-		giving_up_timer.start()
-	
+		if giving_up_timer.is_stopped():
+			giving_up_timer.start()
+
 	var position_difference = player_target.position.x - position.x
 	var target_dir : int = sign(position_difference)
 	if target_dir != direction and is_on_floor() and abs(position_difference) > 10:
 		_change_direction()
 
+	if start_attack:
+		_player_entered_jump_attack_area(null)
+
+func _on_giving_up_timer_timeout() -> void:
+	unnasign_player()
 
 func _on_guarding_state_physics_processing(delta: float) -> void:
 	v_component.set_proper_velocity(walk_speed * direction, 1)
-	
+
 	wall_detector.force_raycast_update()
-	var hit_wall := wall_detector.is_colliding()
 	ground_detector.force_raycast_update()
+
+	var hit_wall := wall_detector.is_colliding()
 	var no_ground := !ground_detector.is_colliding()
-	
+
 	if (hit_wall or no_ground) and is_on_floor():
 		_change_direction()
 
@@ -220,7 +272,6 @@ func _on_guarding_state_physics_processing(delta: float) -> void:
 # ======================
 # MECÂNICAS
 # ======================
-
 func flip() -> void:
 	wall_detector.scale.x *= -1
 	jump_detector.scale.x *= -1
@@ -234,7 +285,6 @@ func flip() -> void:
 	outer_arrow_detec.scale.x *= -1
 	inner_arrow_detec.scale.x *= -1
 
-
 func _change_direction() -> void:
 	if is_changing_direction:
 		return
@@ -242,10 +292,8 @@ func _change_direction() -> void:
 	is_changing_direction = true
 	await get_tree().create_timer(direction_change_delay).timeout
 
-	# só vira depois do delay
 	direction *= -1
 	flip()
-
 	is_changing_direction = false
 
 func _on_outer_detector_entered(_area: Area2D) -> void:
@@ -277,10 +325,10 @@ func avoid_arrow() -> void:
 	inner_detector = false
 	can_dash = true
 
+
 # ======================
 # VIDA
 # ======================
-
 func _on_health_ran_out() -> void:
 	queue_free()
 
