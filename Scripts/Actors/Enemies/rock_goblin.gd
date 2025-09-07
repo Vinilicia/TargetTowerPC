4a552223e15dba12d05dcac322f028cf9a321681
Hawkeye-789 <dyegoeduardo0032@gmail.com>
2025-09-06 22:14:55 -0300

    Diminuição na velocidade da pedra. Persistência no estado de ataque

--- Scripts/Actors/Enemies/rock_goblin.gd
+++ Scripts/Actors/Enemies/rock_goblin.gd
@@ -1,13 +1,13 @@
 extends CharacterBody2D
 
 @export_enum("Left", "Right") var direction : int
-@export var sight_area_scale : Vector2 = Vector2(320, 150)
 
 @onready var rock = preload("res://Scenes/Actors/Enemies/Throwable_rock.tscn")
 @onready var sight_area := $SightArea as Area2D
 @onready var rock_spawner = $RockSpawner as Marker2D
 @onready var look_around_timer = $LookAroundTimer as Timer
 @onready var attack_timer = $AttackTimer as Timer
+@onready var loose_sight_timer = $LooseSightTimer as Timer
 
 var player_is_nearby : bool = false
 var player_target : CharacterBody2D
@@ -15,8 +15,6 @@ var timer_off : bool = false
 var angle : float
 
 func _ready() -> void:
-	sight_area.scale = sight_area_scale
-	
 	if direction == 0:
 		direction = -1
 	else:
@@ -42,15 +40,16 @@ func _throw_rock() -> void:
 func _player_entered_sight_area(player: Node2D) -> void:
 	player_target = player
 	player_is_nearby = true
-	look_around_timer.stop()
-	attack_timer.start()
+	if !look_around_timer.is_stopped():
+		look_around_timer.stop()
+	if attack_timer.is_stopped():
+		attack_timer.start()
+	if !loose_sight_timer.is_stopped():
+		loose_sight_timer.stop()
 
 func _player_exited_sight_area(_player: Node2D) -> void:
-	player_is_nearby = false
-	if attack_timer.is_inside_tree():
-		attack_timer.stop()
-	if look_around_timer.is_inside_tree():
-		look_around_timer.start()
+	if loose_sight_timer.is_inside_tree():
+		loose_sight_timer.start()
 
 func flip() -> void:
 	sight_area.position.x *= -1
@@ -73,3 +72,12 @@ func handle_attack() -> void:
 
 func _on_attack_timer_timeout() -> void:
 	_throw_rock()
+
+func _on_loose_sight_timer_timeout() -> void:
+	player_is_nearby = false
+	if attack_timer.is_inside_tree():
+		attack_timer.stop()
+	print("Anal")
+	if look_around_timer.is_inside_tree():
+		print("Sex")
+		look_around_timer.start()
