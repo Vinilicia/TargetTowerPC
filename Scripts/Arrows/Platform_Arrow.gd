58de26bf6b1746bae83b81dbb24c3f2d05f4eee0
Hawkeye-789 <dyegoeduardo0032@gmail.com>
2025-09-07 23:18:06 -0300

    Plataforma se posiciona com base na superfície de contato

--- Scripts/Arrows/Platform_Arrow.gd
+++ Scripts/Arrows/Platform_Arrow.gd
@@ -1,21 +1,46 @@
 extends Arrow
 
 @onready var plat_spawner = preload("res://Scenes/Interactibles/Objects/Platform.tscn")
+@export var raycast: RayCast2D
 
 var can_spawn : bool = true 
 
-func _on_body_entered(body) -> void:
+func _on_body_entered(body: Node2D) -> void:
 	get_frozen()
 	if can_spawn:
 		can_spawn = false
-		var platform = plat_spawner.instantiate()
-		call_deferred("add_child", platform)
-		platform.position = Vector2(5, -0.5)
-		platform.call_deferred("activate", facing_direction, downward)
+
+		if raycast.is_colliding():
+			var contact_point = raycast.get_collision_point()
+			var normal = raycast.get_collision_normal().normalized()
+
+			var platform = plat_spawner.instantiate()
+			body.call_deferred("add_child", platform)
+			platform.global_position = contact_point - body.global_position
+
+			# Decide orientação pelo vetor normal
+			if abs(normal.y) > abs(normal.x):
+				# É chão ou teto
+				if normal.y < 0:
+					# colisão veio de cima → chão
+					platform.call_deferred("activate", 0, false)
+				else:
+					# colisão veio de baixo → teto
+					platform.call_deferred("activate", 0, true)
+			else:
+				# É parede
+				if normal.x > 0:
+					# colisão veio da direita
+					platform.call_deferred("activate",-1, false)
+				else:
+					# colisão veio da esquerda
+					platform.call_deferred("activate",1, false)
+
+	# lógica original
 	if body.is_in_group("Attachables"):
 		spawn_joint(body)
 		despawn()
-	if !body.is_in_group("Attachables"):
+	else:
 		bounce()
 
 func _on_hitbox_hit(target: Node2D) -> void:
--- Scripts/Interactibles/Objects/Platform.gd
+++ Scripts/Interactibles/Objects/Platform.gd
@@ -13,22 +13,21 @@ func activate(direction: int, downward: bool) -> void:
 	
 	dir = direction
 	down = downward
-	vertical = (direction == 0) # se direction = 0 → vertical, senão horizontal
+	vertical = (direction == 0)
 	
 	$Timer.start(spawning_time)
-	spawn()
 
 func spawn() -> void:
 	var tween = create_tween()
 	if vertical:
+		platform_col.set_deferred("one_way_collision", false)
 		if down: # vindo do teto
-			platform_col.set_deferred("one_way_collision", false)
-			tween.tween_property(platform_col, "scale", Vector2(1.2, -0.2), 0.25)
+			tween.tween_property(platform_col, "scale", Vector2(4, 24), 0.25)
 			tween.parallel().tween_property($Platform, "position", Vector2(0, 12), 0.25)
 		else: # vindo do chão
-			tween.tween_property(platform_col, "scale", Vector2(1.2, -0.2), 0.25)
+			tween.tween_property(platform_col, "scale", Vector2(4, 24), 0.25)
 			tween.parallel().tween_property($Platform, "position", Vector2(0, -12), 0.25)
 	else:
 		# horizontal
-		tween.tween_property(platform_col, "scale", Vector2(1.2, -0.2) * dir, 0.25)
+		tween.tween_property(platform_col, "scale", Vector2(24 * dir, -4), 0.25)
 		tween.parallel().tween_property($Platform, "position", Vector2(12 * -dir, 0), 0.25)
