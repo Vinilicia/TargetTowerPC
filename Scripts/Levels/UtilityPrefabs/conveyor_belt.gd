fb576dd15fae1a28910836042d2f12e6cda73e1c
Hawkeye-789 <dyegoeduardo0032@gmail.com>
2025-09-26 09:19:33 -0300

    Mudança dinamica no tamanho da Windbox e Conveyor Belt com @tool. Sprite da Conveyor belt

--- Scripts/Levels/UtilityPrefabs/conveyor_belt.gd
+++ Scripts/Levels/UtilityPrefabs/conveyor_belt.gd
@@ -1,7 +1,28 @@
+@tool
 extends Node2D
+
+@onready var area : Area2D = $Belt
+@onready var body : StaticBody2D = $StaticBody
+@onready var tilemap : TileMapLayer = $Tilemap
 
 @export var belt_direction : Vector2
 @export var belt_speed : float
+@export_group("Editor")
+@export var length : int = 0:
+	set(new_length):
+		#new_length = clampi(new_length, 3, 1000)
+		area.scale.x = new_length * 16
+		area.position.x = new_length * 8
+		body.scale.x = new_length * 16
+		body.position.x = new_length * 8
+		tilemap.clear()
+		if new_length >= 3:
+			tilemap.set_cell(Vector2i(0, -1), 2, Vector2i(0, 0), 0)
+			for i in range(new_length - 2):
+				tilemap.set_cell(Vector2i(i + 1, -1), 2, Vector2i(0, 1), 0)
+			tilemap.set_cell(Vector2i(new_length - 1, -1), 2, Vector2i(0, 2), 0)
+		length = new_length
+
 
 var bodies_inside : Array[Node2D] = []
 
--- Scripts/Levels/UtilityPrefabs/windbox.gd
+++ Scripts/Levels/UtilityPrefabs/windbox.gd
@@ -1,12 +1,31 @@
+@tool
 extends Node2D
+
+@onready var area : Area2D = $Wind
 
 @export var wind_direction : Vector2
 @export var wind_force : float
+@export_group("Editor")
+
+@export var size : Vector2i = Vector2i(2, 2):
+	set(new_size):
+		new_size = new_size.clampi(2, 1000)
+		area.scale.x = new_size.x * 16
+		area.position.x = new_size.x * 8
+		area.scale.y = new_size.y * 16
+		area.position.y = new_size.y * 8
+		size = new_size
+
+var bodies_inside: Array[Node2D] = []
 
 func _on_wind_body_entered(body: Node2D) -> void:
-	if body is Player:
-		body.v_component.add_wind_velocity(wind_direction * wind_force)
+	var v_component : VelocityComponent = body.find_child("VelocityComponent")
+	if v_component:
+		bodies_inside.append(body)
+		v_component.add_ground_velocity(wind_direction * wind_force)
 
 func _on_wind_body_exited(body: Node2D) -> void:
-	if body is Player:
-		body.v_component.add_wind_velocity(wind_direction * -wind_force)
+	var v_component : VelocityComponent = body.find_child("VelocityComponent")
+	if v_component:
+		bodies_inside.erase(body)
+		v_component.add_ground_velocity(wind_direction * -wind_force)
