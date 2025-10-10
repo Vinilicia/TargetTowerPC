546a1bc93c966e2621693ab80999fa01fb563ec8
Vinícius Souza <viniso1903@gmail.com>
2025-10-10 11:25:28 -0300

    Player spawna no lugar marcado do portal

--- Scripts/Main/game.gd
+++ Scripts/Main/game.gd
@@ -4,8 +4,9 @@ extends Node2D
 
 @export var current_level : Node
 
-func change_level(next_level : String):
+func change_level(next_level : String, spaw_position : Vector2):
 	var new_level = load(next_level).instantiate()
 	call_deferred("remove_child", current_level)
 	call_deferred("add_child", new_level)
+	get_node("Player").global_position = spaw_position
 	current_level = new_level
--- Scripts/Main/portal.gd
+++ Scripts/Main/portal.gd
@@ -1,7 +1,8 @@
 extends Area2D
 
 @export var next_level : String
+@export var spaw_position : Vector2
 
 func _on_body_entered(body: Node2D) -> void:
 	var game : Game = get_parent().get_parent()
-	game.change_level(next_level)
+	game.change_level(next_level, spaw_position)
