5d8a05f52b7d81f750e36383e34767a9bd1cbd3f
Vinícius Souza <viniso1903@gmail.com>
2025-10-10 10:56:18 -0300

    Script do Game e do Portal criado

--- Scripts/Main/game.gd
+++ Scripts/Main/game.gd
@@ -1,1 +1,11 @@
+class_name Game
+
 extends Node2D
+
+@export var current_level : Node
+
+func change_level(next_level : String):
+	var new_level = load(next_level).instantiate()
+	call_deferred("remove_child", current_level)
+	call_deferred("add_child", new_level)
+	current_level = new_level
--- Scripts/Main/portal.gd
+++ Scripts/Main/portal.gd
@@ -1,1 +1,7 @@
 extends Area2D
+
+@export var next_level : String
+
+func _on_body_entered(body: Node2D) -> void:
+	var game : Game = get_parent().get_parent()
+	game.change_level(next_level)
