3fc184cab1945b79cdbe79a1cfa622520f741327
Hawkeye-789 <dyegoeduardo0032@gmail.com>
2025-10-19 21:18:31 -0300

    Inimigos agora não respawnam na sala anterior

--- Scripts/Actors/Enemies/Enemy.gd
+++ Scripts/Actors/Enemies/Enemy.gd
@@ -5,7 +5,10 @@ class_name Enemy
 @export var v_component : VelocityComponent
 @export var health_man : HealthManager
 
+signal died
+
 func die() -> void:
+	died.emit()
 	queue_free()
 	
 func took_damage(_amount : float) -> void:
--- Scripts/Main/portal.gd
+++ Scripts/Main/portal.gd
@@ -4,5 +4,5 @@ extends Area2D
 @export var spaw_position : Vector2
 
 func _on_body_entered(_body: Node2D) -> void:
-	var game : Game = get_parent().get_parent()
+	var game : Game = get_tree().get_first_node_in_group("Game")
 	game.change_level(next_level, spaw_position)
