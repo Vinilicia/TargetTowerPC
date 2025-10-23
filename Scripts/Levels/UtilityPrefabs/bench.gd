00a3969c0f1680535ebf8ad16fe8534e8c7c475c
Vinícius Souza <viniso1903@gmail.com>
2025-10-23 10:43:30 -0300

    Script do bench atualizado

--- Scripts/Levels/UtilityPrefabs/bench.gd
+++ Scripts/Levels/UtilityPrefabs/bench.gd
@@ -1,11 +1,13 @@
 extends Node2D
 
-var player : Player = null
+@export var bench_id: int = 0
 
-func _physics_process(delta: float) -> void:
-	if player:
-		if Input.is_action_just_pressed("down"):
-			bench_used()
+var player: Player = null
+var SaveLoad
+
+func _physics_process(_delta: float) -> void:
+	if player and Input.is_action_just_pressed("down"):
+		bench_used()
 
 func _on_player_entered(body: Node2D) -> void:
 	if body is Player:
@@ -16,4 +18,16 @@ func _on_player_exited(body: Node2D) -> void:
 		player = null
 
 func bench_used() -> void:
-	print("I sit on the toilet")
+	print("Jogador sentou no banco ID:", bench_id)
+	
+	# Atualiza o ID do último banco usado
+	SaveLoad.SaveFileData.set_last_bench_id(bench_id)
+	
+	# Aqui você pode atualizar outras coisas do jogador, se quiser
+	# Ex: SaveLoad.SaveFileData.set_money(player.money)
+	# Ex: SaveLoad.SaveFileData.set_max_health(player.max_health)
+	
+	# Salva o jogo no slot atual (por exemplo, 0)
+	SaveLoad._save(0)
+	
+	print("Jogo salvo com sucesso no banco", bench_id)
