0caf70c255d5f54c5f365328857024e4e3629359
Hawkeye-789 <dyegoeduardo0032@gmail.com>
2025-10-24 11:35:59 -0300

    Correções de erros com transição de fase

--- Scripts/Menus&UI/title_screen.gd
+++ Scripts/Menus&UI/title_screen.gd
@@ -65,7 +65,7 @@ func save_file_3_button_pressed() -> void:
 func load_save(save_id) -> void:
 	var save_load_manager : SaveLoadManager = SaveLoadManager.new()
 	save_load_manager._load(save_id)
-	var scene_path := "res://Scenes/Levels/Areas/EntranceHall/Room%d.tscn" % save_load_manager.save_file_data.get_last_bench_id()
+	var scene_path := "res://Scenes/Levels/Areas/Area1/Room%d.tscn" % save_load_manager.save_file_data.get_last_bench_id()
 	var room_scene: PackedScene = load(scene_path)
 	visible = false
 	game.save_id = save_id
